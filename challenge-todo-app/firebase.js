5d7e3809adc0c670a5b0352a05d3808ae69f295c
Shivam Kumar <66867825+shivamsince0502@users.noreply.github.com>
2022-05-31 23:02:03 +0530

    Add files via upload
    Initial commit

---
+++ challenge-todo-app/firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAsxvKaJtna9t-fZ9ttg0Qtyz2UJT0IJKY",
+  authDomain: "to-do-a-d0248.firebaseapp.com",
+  projectId: "to-do-a-d0248",
+  storageBucket: "to-do-a-d0248.appspot.com",
+  messagingSenderId: "300581901538",
+  appId: "1:300581901538:web:19765d33048d0f38d23abb",
+  measurementId: "G-VJ986H5B8S"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore();
+
---
+++ challenge-todo-app/plan.md
@@ -0,0 +1,8 @@
+1. Create the boxes we need to create
+2. Create the banner background image 
+3. Create a header
+4. Create the inpur section
+5. Create teh to-do Items list section
+6. Set up our database
+8. Create the functionality to grab the data from database
+9. Create the functionality to update the data with new status of completetd or active
